de2122a64e3263409d390a53114e7fb335a6f489
yogisiswanto <yogisiswanto.c2@gmail.com>
2022-03-31 22:41:17 +0700

    Add section 2

---
+++ Section 2 - Basics Variable, Data Types, Operators & Functions/Assignment/basics-assignment-1-start/assignment.js
@@ -0,0 +1,17 @@
+const userInput = 2;
+let result;
+
+// Add
+result = 18 + userInput;
+
+// Substraction
+result = 18 - userInput;
+
+// Multiplication
+result = 18 * userInput;
+
+// Divider
+result = 18 / userInput;
+
+alert(userInput);
+alert(result);
